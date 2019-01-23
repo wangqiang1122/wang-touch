233d13d268e602a7b324ffb3876990d3d57697c6
wangqiang1122 <wangqiangle@163.com>
2019-01-23 14:36:13 +0800

    111

--- fishing/js/bullet.js
+++ fishing/js/bullet.js
@@ -1,3 +1,13 @@
-function Bullut() {
-    Sprit.call();
-}
+function Bullut(type) {
+    var bottombar = _g_res_src['bullet']['bullet'+type];
+    console.log(bottombar.img)
+    Sprit.call(this,{
+        img: bottombar.img,
+        width: bottombar.frame.w,
+        height: bottombar.frame.h,
+        sx: bottombar.frame.x,
+        sy: bottombar.frame.y,
+        speed: 8,
+    });
+};
+resetConstructor(Bullut,Sprit)
