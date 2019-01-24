22eb4d3b1bc64345eaa776a86bd0fa740cd78175
admin <admin@admindeMacBook-Pro.local>
2019-01-24 22:17:48 +0800

    1111

---
+++ EventEimtter/EventEimtter.js
@@ -0,0 +1,8 @@
+const event = require('events').EventEmitter;
+let ev = new event();  // 队列可以解耦合 不关系谁去接受 有几个人接受 接收去干嘛
+console.log(ev)
+
+ev.on('blur',function (a,b) {
+    console.log(a,b)
+});
+ev.emit('blur',1,2)
