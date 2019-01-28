78cc812254eb6947ac100ed40cdbb951cb8cdac2
admin <admin@admindeMacBook-Pro.local>
2019-01-28 21:34:36 +0800

    dns111

--- dns/dns.js
+++ dns/dns.js
@@ -1,4 +1,8 @@
 let dns = require('dns');
-dns.lookup('www.baidu.com',(err,data)=>{
+dns.lookup('www.nodejs.org',(err,data)=>{
     console.log(data)
-})
+});
+dns.lookupService('104.20.22.46',80,(err,data)=>{
+    console.log(err)
+    console.log(data)
+});
