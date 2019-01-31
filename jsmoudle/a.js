7a0873a3a371877b0facca68798edb3e96da6d94
admin <admin@admindeMacBook-Pro.local>
2019-01-31 21:21:21 +0800

    11111

--- jsmoudle/a.js
+++ jsmoudle/a.js
@@ -2,4 +2,8 @@
 // export const b = function () {
 //     console.log('cccc')
 // }
-console.log('a')
+// console.log('a');
+import { a, arr } from './b.js'
+import './c.js';
+console.log(a);
+arr(new Date().getTime())
--- jsmoudle/b.js
+++ jsmoudle/b.js
@@ -1,1 +1,5 @@
-console.log('b')
+// console.log('b')
+export const a= '1';
+export function arr(data) {
+    console.log(data)
+}
