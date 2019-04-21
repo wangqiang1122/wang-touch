74e3d5aadcfd296faa9ce72853b122d1cf9382a7
admin <admin@admindeMacBook-Pro.local>
2019-04-21 23:17:10 +0800

    新加

--- angular/myApp/src/app/app.module.ts
+++ angular/myApp/src/app/app.module.ts
@@ -5,14 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  // 声明 组件放声明
   declarations: [
     AppComponent
   ],
+  //倒入 模块放倒入
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] // 启动
 })
 export class AppModule { }
