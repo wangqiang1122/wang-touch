0bc4d5834c8c801ef8b10acea22d13628d24492b
wangqiang1122 <wangqiangle@163.com>
2019-04-22 18:30:26 +0800

    新家

--- koa/koa1.js
+++ koa/koa1.js
@@ -7,10 +7,11 @@ server.listen(8080);
 
 
 // 接口
-server.use(async function (req,res) {
-});
+// server.use(async function (req,res) {
+// });
 
 
 // 静态文件
 server.use(static('www'));
+///key = key.replace(/<\/?.+?>/g,""); // （前面要有)< (一个或者0个)/ 任意字符(最少一个多则不限)  >(后面有一个)
 
