a7e880808154a212ed7e7d31fb67ae2fa2614e52
wangqiang1122 <wangqiangle@163.com>
2019-05-09 18:12:56 +0800

    1111

--- dns/dns.js
+++ dns/dns.js
@@ -2,7 +2,7 @@ let dns = require('dns');
 dns.lookup('www.cnblogs.com',(err,data)=>{
     console.log(data)
 });
-dns.lookupService('101.37.225.65',80,(err,data)=>{
+dns.lookupService('47.111.45.248',80,(err,data)=>{
     console.log(err)
     console.log(data)
 });
--- koa-ejs/app.js
+++ koa-ejs/app.js
@@ -1,7 +1,19 @@
 const koa = require('koa');
 const server = new koa();
 const ejs = require('koa-ejs');
+const mysql = require('./lib/mysql');
+const dbs = mysql.createPool({ host: 'localhost', user: 'root', password: '12345678',database: 'an_ju_ke'});
+
 server.listen('3333');
 ejs(server,{
+    root: './telemlate',
+    layout: false,
+    viewExt: 'html',
+    cache: false,
+    debug: true
+});
 
+server.use(async (ctx)=>{
+    let a = await dbs.query('Select * from house_table');
+    await ctx.render('a',{ ArrMane: a })
 });
