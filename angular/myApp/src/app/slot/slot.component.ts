935e5c5c0b607889e4e15b75e2ba24f54034c22f
wangqiang1122 <wangqiangle@163.com>
2019-05-17 18:27:14 +0800

    3333

---
+++ angular/myApp/src/app/slot/slot.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slot',
+  templateUrl: './slot.component.html',
+  styleUrls: ['./slot.component.css']
+})
+export class SlotComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
