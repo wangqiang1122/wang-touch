60eb44fcc0c9c559f35cf382676dff2dcfe940ef
admin <admin@admindeMacBook-Pro.local>
2019-11-05 21:44:32 +0800

    对单件的初步理解

---
+++ 设计模式/单件模式.js
@@ -0,0 +1,20 @@
+// class Clo {
+//     static d() {
+//
+//     }
+//
+//     se() {}
+// }
+//
+//
+// var a = new Clo();
+// console.log(a);
+
+
+function Clo() {
+
+}
+Clo.d = function () {
+
+};
+
