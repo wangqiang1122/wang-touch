0a72c4491a7bd191eee4f408009e2d90f2837bab
wangqiang1122 <wangqiangle@163.com>
2019-11-05 19:01:06 +0800

    抽象工厂模式es61

--- 设计模式/生成器之抽象工厂方法-es6.js
+++ 设计模式/生成器之抽象工厂方法-es6.js
@@ -1,4 +1,4 @@
-// 抽象工厂方法就只只写接口 不写任何业务逻辑
+// 抽象工厂方法就只只写接口 不写任何业务逻辑 适合大型项目 所有的分部的披萨原料工厂 都要继承次总部的原料工厂
 class PizzaIngredientFoctory{
     createDough(){};
     createSauce(){};
@@ -6,4 +6,7 @@ class PizzaIngredientFoctory{
     createPepperoni(){};
     createClam(){};
 }
+// 纽约的原料工厂
+class NYPizzaIngredientFoctory extends  PizzaIngredientFoctory{
 
+}
