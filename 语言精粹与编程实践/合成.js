087045291612d0b8c5d3d32e59f7331f3ccc1260
admin <admin@admindeMacBook-Pro.local>
2020-09-11 09:24:42 +0800

    测试demo

--- 语言精粹与编程实践/合成.js
+++ 语言精粹与编程实践/合成.js
@@ -1,5 +1,9 @@
-import * as myName from './a.js'; // 只适合webpack方式es6方式暂时不支持
+// import * as myName from './a.js'; // 只适合webpack方式es6方式暂时不支持
 
 
 // myName.f='222'
-console.log(myName)
+// console.log(myName);
+var {a=100} = {};
+console.log({100:a=100});
+console.log(a)
+console.log({100:a=100}={100:a=100})
