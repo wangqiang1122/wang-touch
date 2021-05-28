af94f5845feb0a8c18a7156994e64c92311630ed
wangqiang_rc <wangqiangle@163.com>
2021-05-28 10:47:58 +0800

    demo

--- 聚合函数/index.js
+++ 聚合函数/index.js
@@ -12,44 +12,45 @@
     // console.log(a())
     // console.log(a)
 
-    // function f1(args) {
-    //     console.log('f1');
-    //     return args
-    // }
-    // function f2(args) {
-    //     console.log('f2');
-    //     return args
-    // }
-    // function f3(args) {
-    //     console.log('f3');
-    //     return args
-    // }
-    // function f4(args) {
-    //     console.log('f4');
-    //     return args
-    // }
-    // compose(f1,f2,f3,f4)('gggg')
-    // function compose(...fnc) {
-    //   return ()=> {
-    //       return fnc.reduce((a,b)=>{
-    //           console.log('compose'+a);
-    //           console.log('compose'+b)
-    //           return (...args)=>{
-    //               console.log(a);
-    //               console.log(b)
-    //               return a(()=>b(...args))()
-    //           }
-    //       })()
-    //   }
-    // }
-    let next =async () => {}
+    function f1(args) {
+        console.log('f1');
+        return args
+    }
+    function f2(args) {
+        console.log('f2');
+        return args
+    }
+    function f3(args) {
+        console.log('f3');
+        return args
+    }
+    function f4(args) {
+        console.log('f4');
+        return args
+    }
+    compose(f1,f2,f3,f4)()
+    function compose(...fnc) {
+      return ()=> {
+          return fnc.reduce((a,b)=>{
+              console.log('compose'+a);
+              console.log('compose'+b)
+              return (...args)=>{
+                  console.log(a);
+                  console.log(b)
+                  return a(()=>b(...args))()
+              }
+          })()
+      }
+    }
+    // let next =async () => {}
 
     let g =  function(middlewares) {
+        // console.log(middlewares[0]())
         let idx = 0
         async function next() {
-            if (idx < middlewares.length) return Promise.resolve(middlewares[idx++](next))
+            if (idx < middlewares.length) return  Promise.resolve(middlewares[idx++](next))
         }
-        return next
+        return next()
         // return ()=> {
         //     // return middlewares.reduce((a,b)=>{
         //     //     console.log('compose'+a);
@@ -72,25 +73,26 @@
     async function fn1(next) {
         console.log(next)
         console.log("fn1")
-        // return args
+        return next
         // await next();
-        // console.log('fn1-222')
+        console.log('fn1-222')
     }
     async function fn2(next) {
         console.log(next)
         console.log("fn2")
-        // return args
+        return next
         // await next()
         console.log('fn2---h')
     }
     async function fn3(next) {
         console.log("fn3")
         // await next()
+        return next
     }
     async function fn4(next) {
         console.log("fn4")
-        // await next()
+        await next()
     }
     // fn1(fn2(fn3()))
-    console.log(g([fn1,fn2,fn3,fn4]))
+    // console.log(g([fn1,fn2,fn3,fn4]))
 })()
